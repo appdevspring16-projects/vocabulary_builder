4024e97d93ba3b2ebb02fda3763b8f396dc6ed10
Raghu Betina <raghu@firstdraft.com>
2017-01-19 19:41:27 +0000

    Whitelist columns in Word for ActiveAdmin

--- app/admin/word.rb
+++ app/admin/word.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Word do
 
+ permit_params :word, :language, :definition
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
