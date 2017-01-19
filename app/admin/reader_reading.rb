6d95855bd1a58d75187c262eacf343b4e16d2565
Raghu Betina <raghu@firstdraft.com>
2017-01-19 19:41:37 +0000

    Whitelist columns in ReaderReading for ActiveAdmin

--- app/admin/reader_reading.rb
+++ app/admin/reader_reading.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ReaderReading do
 
+ permit_params :reader_id, :reading_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
