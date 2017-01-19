3322823acc147ae7d5130b28e80b6335d47e57af
Raghu Betina <raghu@firstdraft.com>
2017-01-19 19:41:03 +0000

    Direct association between reader_readings and reader_words

--- app/models/reader_reading.rb
+++ app/models/reader_reading.rb
@@ -1,6 +1,10 @@
 class ReaderReading < ApplicationRecord
   # Direct associations
 
+  has_many   :reader_words,
+             :foreign_key => "reader_readings_id",
+             :dependent => :destroy
+
   belongs_to :reader
 
   # Indirect associations
--- app/models/reader_word.rb
+++ app/models/reader_word.rb
@@ -1,6 +1,8 @@
 class ReaderWord < ApplicationRecord
   # Direct associations
 
+  belongs_to :reader_readings
+
   belongs_to :word
 
   belongs_to :reader
